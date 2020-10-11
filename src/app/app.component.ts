013e2e76351de0217331fecf1de284f696ce00bc
BlankSheetBGH <hynd.brixigormat@gmail.com>
2020-10-11 18:43:03 +0100

    first commir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-material-design-tp';
+  
+  
+  allerSimple = false;  
+  coef= 2;
+  coutAlgerMontreal : number;
+  coutAlgerTataouine : number;
+  coutAlgerTokyo : number;
+  coutCourrier : number;
+  departureCities = ['Alger'];
+  destinationCities = ['Montreal','Tataouine Les Bains', 'Tokyo'];
+  devis= 0;      
+  minDate = new Date();
+  //maxDate = new Date(2029, 9, 20); 20 octobre 2029  
+  testing:string;
+  devisForm: FormGroup;
+  marche = 12;
+
+  ngOnInit(){                
+    this.coutAlgerMontreal = 90000;   
+    this.coutAlgerTataouine = 13000;
+    this.coutAlgerTokyo = 150000;    
+    this.coutCourrier = 0;
+    this.testing= "";
+    
+
+    this.devisForm= new FormGroup ({
+      'depart': new FormControl('null', Validators.required),
+      'destinations': new FormControl ('null', Validators.required),
+      'aller': new FormControl ('null', Validators.required),
+      'retour': new FormControl ('null'),
+      'personnes': new FormControl (1)
+      
+    });
+  }
+   
+
+  //determiner le cout d'un aller simple selon destination
+  coutCourrierCalc(){
+    this.marche++;
+    if (this.devisForm.get("destinations").value == "Montreal") {
+      this.coutCourrier=this.coutAlgerMontreal;
+    }   
+    else if(this.devisForm.get("destinations").value == "destinationCities[1]"){
+      this.coutCourrier=this.coutAlgerTataouine;
+    }
+    else if(this.devisForm.get("destinations").value == "Tokyo"){
+      this.coutCourrier=this.coutAlgerTokyo;
+    }
+
+  }
+
+
+  // Aller simple/aller-retour
+  toggleAllerSimple() {
+      this.allerSimple = !this.allerSimple;   
+      if (this.allerSimple = false) {
+        this.coef = 2;
+      }   
+      else {
+        this.coef=1;
+      }
+  }
+    
+  //count number of persons
+  increment() {
+    this.devisForm.setValue({
+      personnes: this.devisForm.get("personnes").value + 1
+    });
+  }
+  
+  decrement() {
+    this.devisForm.setValue({
+      personnes: this.devisForm.get("personnes").value - 1
+    });
+  }
+
+  //Calcul du devis
+  calculDevis() {
+    this.devis= this.devisForm.get("personnes").value*this.coef*this.coutCourrier;
+
+
+
+    this.testing = this.devisForm.get("destinations").value;
+  }
+
+  
+
+  
+
 }
